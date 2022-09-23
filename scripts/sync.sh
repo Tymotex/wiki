d00876237f0d50e78963fdf0c10a23260fd53e0a
Tim Zhang <timzhang3@gmail.com>
2022-09-23 16:42:01 +1000

    Git Sync: 2022-09-23 16:42:01

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git pull
-git add . && git commit -m "Git Sync: $(date +%Y-%m-%d %H:%M:%S)" && git push
+git add . && git commit -m "Git Sync: $(date +'%Y-%m-%d %H:%M:%S')" && git push
 git pull
 
 # Note: we git pull twice because the first might fail due to unmerged changes.
